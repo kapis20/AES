1ac5726503f26467b699d206334dc9f75d484199
kapis20 <kacper.filip.sikorski@gmail.com>
2023-11-03 17:57:18 +0000

    ShiftRows_added - for some reason it was not on Git

---
+++ ShiftRows.m
@@ -0,0 +1,11 @@
+function [output] = ShiftRows(input)
+    
+    %Shifts the rows of the matrix by a pre-determined amount
+
+    output(1,:) = input(1,:);                       %Row 1
+    output(2,:) = circshift(input(2,:), [0 -1]);    %Row 2
+    output(3,:) = circshift(input(3,:), [0 -2]);    %Row 3
+    output(4,:) = circshift(input(4,:), [0 -3]);    %Row 4
+
+end
+
