d089dc0563a831124e078a61541f3af733b9560c
chtbaldwin2 <101398947+chtbaldwin2@users.noreply.github.com>
2024-02-05 14:13:19 +0000

    Completed Shift Rows Blocks
    Include Shift Rows, Inverse Shift Rows and a testbench which tests both

--- Verilog/invShiftRows.v
+++ Verilog/invShiftRows.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module shiftRows(clk, data_in, data_out);
+module invShiftRows(clk, data_in, data_out);
 
     input clk;
     input [127:0] data_in;
@@ -16,10 +16,10 @@ module shiftRows(clk, data_in, data_out);
         data_out[103:96]<= data_in[103:96];
         
         // Row 2
-        data_out[15:8]<= data_in[47:40];
-        data_out[47:40]<=data_in[79:72];
-        data_out[79:72]<=data_in[111:104];
-        data_out[111:104]<=data_in[15:8];
+        data_out[15:8]<= data_in[111:104];
+        data_out[47:40]<=data_in[15:8];
+        data_out[79:72]<=data_in[47:40];
+        data_out[111:104]<=data_in[79:72];
         
         // Row 3
         data_out[23:16]<=data_in[87:80];
@@ -28,10 +28,10 @@ module shiftRows(clk, data_in, data_out);
         data_out[119:112]<=data_in[55:48];
         
         // Row 4
-        data_out[31:24]<=data_in[127:120];
-        data_out[63:56]<=data_in[31:24];
-        data_out[95:88]<=data_in[63:56];
-        data_out[127:120]<=data_in[95:88];
+        data_out[31:24]<=data_in[63:56];
+        data_out[63:56]<=data_in[95:88];
+        data_out[95:88]<=data_in[127:120];
+        data_out[127:120]<=data_in[31:24];
 
     end
 
--- Verilog/tb_shiftRows.v
+++ Verilog/tb_shiftRows.v
@@ -23,13 +23,23 @@
 module tb_shiftRows;
 
     reg clk = 0;
-    reg [7:0] data_in = 8'b10101010;
-    wire [7:0] data_out;
+    
+    reg [127:0] data_in = 128'b00001111000011110000111100001111_10010000110011111001000011001111_00001111000011110000111100001111_10010000110011111001000011001111;
+    wire [127:0] data_out;
+    
+    reg [127:0] data_in_i;
+    wire [127:0] data_out_i;
 
-    shiftRows UUT (
+    shiftRows UUT_1 (
         .clk(clk),
         .data_in(data_in),
         .data_out(data_out)
+    );
+    
+    invShiftRows UUT_2 (
+        .clk(clk),
+        .data_in(data_in_i),
+        .data_out(data_out_i)
     );
     
     always #5 clk = !clk;
@@ -38,8 +48,24 @@ module tb_shiftRows;
     
         $display("Starting Test!");
         
-        $display(data_in);
-        $display(data_out);
+        $display("===ENCRYPT===");
+        $display("Data In:  %b", data_in);  
+        #10
+        $display("Data Out: %b", data_out);
+        
+        data_in_i = data_out;
+        
+        $display("===DECRYPT===");
+        $display("Data In:  %b", data_in_i);  
+        #10
+        $display("Data Out: %b", data_out_i);
+        
+        if (data_in == data_out_i) begin
+            $display("Input = Output");
+        end
+        
+        
+        $display("Test Complete!");
         
     end
     
