da182ffd1369ba8811315c820900b20423160967
BoonKean18 <121259353+BoonKean18@users.noreply.github.com>
2024-03-15 11:00:39 +0000

    Add files via upload

---
+++ Verilog/OptimisedAES/mux.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 10:34:55
+// Design Name: 
+// Module Name: mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux(s1, in1, in2, out);
+
+input s1;
+input [7:0] in1, in2;
+output [7:0] out;
+
+
+assign out = s1 ? in2 : in1;
+endmodule
---
+++ Verilog/OptimisedAES/mux2.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 10:40:51
+// Design Name: 
+// Module Name: mux2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2(s1, s2, in1, in2, in3, in4, out);
+
+input s1, s2;
+input [7:0] in1, in2, in3, in4;
+output [7:0] out;
+
+assign out = s1 ? (s2 ? in4 : in3) : (s2 ? in2 : in1);
+
+
+
+endmodule
