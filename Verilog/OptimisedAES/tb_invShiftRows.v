521829a8a88b0aa17ca42cd5363bd0e88e63fc2d
BoonKean18 <121259353+BoonKean18@users.noreply.github.com>
2024-03-16 14:03:53 +0000

    Update tb_invShiftRows.v

--- Verilog/OptimisedAES/tb_invShiftRows.v
+++ Verilog/OptimisedAES/tb_invShiftRows.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 14.03.2024 23:18:33
+// Create Date: 16.03.2024 13:02:20
 // Design Name: 
-// Module Name: tb_shiftrows
+// Module Name: tb_invShiftRows
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -18,7 +18,6 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_invShiftRows();
   // Input signals
@@ -41,21 +40,21 @@ module tb_invShiftRows();
     #5
     inBits = 8'b11010100; //d4
     #10
-    inBits = 8'b00100111; //bf
+    inBits = 8'b10111111; //bf
     #10
-    inBits = 8'b00010001; //5d
+    inBits = 8'b01011101; //5d
     #10
-    inBits = 8'b10101110; //30
+    inBits = 8'b00110000; //30
     
     
     #10
     inBits = 8'b11100000; //e0
     #10
-    inBits = 8'b10111111; //b4
+    inBits = 8'b10110100; //b4
     #10
-    inBits = 8'b10011000; //52
+    inBits = 8'b01010010; //52
     #10
-    inBits = 8'b11110001; //ae
+    inBits = 8'b10101110; //ae
     
     
     
@@ -63,11 +62,11 @@ module tb_invShiftRows();
     #10
     inBits = 8'b10111000; //b8
     #10
-    inBits = 8'b10110100; //41
+    inBits = 8'b01000001; //41
     #10
-    inBits = 8'b01011101; //11
+    inBits = 8'b00010001; //11
     #10
-    inBits = 8'b11100101; //f1
+    inBits = 8'b11110001; //f1
     
     
     
@@ -75,51 +74,51 @@ module tb_invShiftRows();
     #10
     inBits = 8'b00011110; //1e
     #10
-    inBits = 8'b01000001; //27
+    inBits = 8'b00100111; //27
     #10
-    inBits = 8'b01010010; //98
+    inBits = 8'b10011000; //98
     #10
-    inBits = 8'b00110000; //e5
+    inBits = 8'b11100101; //e5
     
     
     //second matrix
     #10                       
     inBits = 8'b01001001; //49
     #10                       
-    inBits = 8'b11011110; //db
+    inBits = 8'b11011011; //db
     #10                       
-    inBits = 8'b11010010; //87
+    inBits = 8'b10000111; //87
     #10                       
-    inBits = 8'b10001001; //3b
+    inBits = 8'b00111011; //3b
     
     
      #10                       
      inBits = 8'b01000101; //45
      #10                       
-     inBits = 8'b11011011; //39
+     inBits = 8'b00111001; //39
      #10                       
-     inBits = 8'b10010110; //53
+     inBits = 8'b01010011; //53
      #10                       
-     inBits = 8'b11110001; //89
+     inBits = 8'b10001001; //89
     
     
      #10                       
      inBits = 8'b01111111; //7f
      #10                       
-     inBits = 8'b00111001; //02
+     inBits = 8'b00000010; //02
      #10                       
-     inBits = 8'b10000111; //d2
+     inBits = 8'b11010010; //d2
      #10                       
-     inBits = 8'b00011010; //f1
+     inBits = 8'b11110001; //f1
      
      #10                       
      inBits = 8'b01110111; //77
      #10                       
-     inBits = 8'b00000010; //de
+     inBits = 8'b11011110; //de
      #10                       
-     inBits = 8'b01010011; //96
+     inBits = 8'b10010110; //96
      #10                       
-     inBits = 8'b00111011; //1a
+     inBits = 8'b00011010; //1a
     
     
     #150
