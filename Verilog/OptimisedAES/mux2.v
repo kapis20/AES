b40de7866736aea3a21e49d44977ad99c06c8580
BoonKean18 <121259353+BoonKean18@users.noreply.github.com>
2024-03-20 02:10:36 +0800

    Cleaned up and added comments

--- Verilog/OptimisedAES/mux2.v
+++ Verilog/OptimisedAES/mux2.v
@@ -1,32 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.03.2024 10:40:51
-// Design Name: 
+// Engineer: Boon Kean Teo
 // Module Name: mux2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Project Name: AES (EEE6225)
+// Description: This module will choose one of the 4 inputs as output. 
 //////////////////////////////////////////////////////////////////////////////////
-
 
 module mux2(s1, s2, in1, in2, in3, in4, out);
 
+//Declaring the inputs and outputs. S1 and S2 are the select bits
 input s1, s2;
 input [7:0] in1, in2, in3, in4;
 output [7:0] out;
 
-
+//When S1 is 0, S2 is 0, then the first input, in1 is assigned as output
+//When S1 is 0, S2 is 1, then the second input, in2 is assigned as output
+//When S1 is 1, S2 is 0, then the third input, in3 is assigned as output
+//When S1 is 1, S2 is 1, then the fourth input 1, in4 is assigned as output
 assign  out = s1 ? (s2 ? in4 : in3) : (s2 ? in2 : in1);
 
 endmodule
