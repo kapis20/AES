c059fb3102cb683446e4fec1198343f4d7012b22
BoonKean18 <121259353+BoonKean18@users.noreply.github.com>
2024-03-20 02:06:06 +0800

    Cleaned up and added comments

--- Verilog/OptimisedAES/mux.v
+++ Verilog/OptimisedAES/mux.v
@@ -1,32 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.03.2024 10:34:55
-// Design Name: 
+// Engineer: Boon Kean Teo
 // Module Name: mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Project Name: AES (EEE6225)
+// Description: This module will choose one of the 2 inputs as output
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module mux(s1, in1, in2, out);
 
+//Declaring the inputs and outputs. S1 is the select bit
 input s1;
 input [7:0] in1, in2;
 output [7:0] out;
 
-
+//When S1 is 1, then the first input 1, in1 is assigned as output and vice versa
 assign out = s1 ? in1 : in2;
 
 endmodule
